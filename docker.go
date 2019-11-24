ffe820490a7cd1be65686cdb1c009a3e3fcfeadb
John Claro <jkrclaro@gmail.com>
2019-11-24 15:53:20 +0000

    Remove all running containers

--- docker.go
+++ docker.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"time"
+	"context"
+	"fmt"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
 )
 
@@ -10,9 +13,25 @@ func docker() *cobra.Command {
 	return &cobra.Command{
 		Use: "docker",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			now := time.Now()
-			prettyTime := now.Format(time.RubyDate)
-			cmd.Println("Hey there!", prettyTime)
+			ctx := context.Background()
+			cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+			if err != nil {
+				panic(err)
+			}
+
+			containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+			if err != nil {
+				panic(err)
+			}
+
+			for _, container := range containers {
+				fmt.Println("Stopping container: ", container.ID[:10], "...")
+				if err := cli.ContainerStop(ctx, container.ID, nil); err != nil {
+					panic(err)
+				}
+				fmt.Println("Successfully removed container: ", container.ID[:10], "...")
+			}
+
 			return nil
 		},
 	}
