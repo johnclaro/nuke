fd9253905515b3eaec332f12e961147db5baaf78
John Claro <jkrclaro@gmail.com>
2019-11-24 17:57:18 +0000

    Print where error occured

--- docker.go
+++ docker.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
 )
@@ -19,13 +20,24 @@ func docker() *cobra.Command {
 				panic(err)
 			}
 
-			containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+			containerListOptions := types.ContainerListOptions{All: true}
+			containers, err := cli.ContainerList(ctx, containerListOptions)
 			if err != nil {
+				fmt.Println("Container list error")
 				panic(err)
 			}
 
 			for _, container := range containers {
 				if err := cli.ContainerStop(ctx, container.ID, nil); err != nil {
+					fmt.Println("Container stop error")
+					panic(err)
+				}
+
+				containerRemoveOptions := types.ContainerRemoveOptions{
+					Force: true,
+				}
+				if err := cli.ContainerRemove(ctx, container.ID, containerRemoveOptions); err != nil {
+					fmt.Println("Container remove error")
 					panic(err)
 				}
 				fmt.Println(container.ID[:12])
@@ -33,6 +45,7 @@ func docker() *cobra.Command {
 
 			images, err := cli.ImageList(ctx, types.ImageListOptions{})
 			if err != nil {
+				fmt.Println("Image list error")
 				panic(err)
 			}
 
@@ -40,9 +53,24 @@ func docker() *cobra.Command {
 				imageRemoveOptions := types.ImageRemoveOptions{Force: true, PruneChildren: true}
 				id, err := cli.ImageRemove(ctx, image.ID, imageRemoveOptions)
 				if err != nil {
+					fmt.Println("Image remove error")
 					panic(err)
 				}
-				fmt.Println("Deleted: sha256:", id[:64])
+				fmt.Println("Deleted: sha256:", id[0].Deleted)
+			}
+
+			volumeArgs := filters.Args{}
+			volumes, err := cli.VolumeList(ctx, volumeArgs)
+			if err != nil {
+				panic(err)
+			}
+
+			for _, volume := range volumes.Volumes {
+				if err := cli.VolumeRemove(ctx, volume.Name, true); err != nil {
+					fmt.Println("Volume remove error")
+					panic(err)
+				}
+				fmt.Println(volume.Name)
 			}
 
 			return nil
